379df0682a089e76c683ec973c0ff454eb360ab6
Veetinho <comicbookguy.jeffrey@gmail.com>
2025-01-06 23:20:37 +0100

    added status pending

--- script.js
+++ script.js
@@ -281,7 +281,7 @@ function getDataFromForm(form) {
   const obj = {}
   for (const [key, value] of formData.entries()) obj[key] = value
   obj.modifiedAt = Date.now()
-  obj.status = 'Oczekuje'
+  obj.status = 'W trakcie'
   return obj
 }
 
@@ -330,6 +330,7 @@ function createApplicationCard(data) {
       <div class="flex flex-col justify-between">
         <div class="flex gap-2 px-2 py-1 items-center bg-${colors[0]}-200 rounded-full">
           <div class="w-3 h-3 rounded-full bg-${colors[0]}-900"></div>
+          ${data.status === 'W trakcie' ? '<svg class="animate-spin h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>' : ''}
           <p class="text-sm font-sans font-semibold">${data.status}</p>
         </div>
         <div>
